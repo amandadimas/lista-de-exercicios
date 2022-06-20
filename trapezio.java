c2e05eb935087f76cceda646b9713f7c89893927
Amanda Dimas Pessoa <107128558+amandadimas@users.noreply.github.com>
2022-06-20 14:30:26 +0300

    Add files via upload

---
+++ trapezio.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class trapezio{
+public static void main(String[] args) {
+  
+
+    Scanner input = new Scanner(System.in);
+
+        Double B,b,h,A;
+      
+        System.out.println("Informe o valor de B = base maior: ");
+        B = input.nextDouble();
+        System.out.println("Informe o valor de b = base menor: ");
+        b = input.nextDouble();
+        System.out.println("Informe o valor de h = altura");
+        h = input.nextDouble();
+       
+        A = ((B + b)/2)*h;
+        System.out.println("O valor dado é B: "+B+" b: "+b+" h: "+h);
+        System.out.println("A área do trapézio é: "+A);
+        input.close();
+    }
+
+}
